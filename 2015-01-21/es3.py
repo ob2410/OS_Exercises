65a9ba811bf631303a6a88300f7e7ccf9c614186
ob2410 <olga2410@ymail.com>
2016-04-18 20:44:18 +0200

    Update es3.py

--- 2015-01-21/es3.py
+++ 2015-01-21/es3.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+'''Scrivere un programma python o uno script bash che scandisca il sottoalbero relativo alle directory passate come
+parametri (o alla direcotry corrente se non ci sono parametri) e fornisca in output l'elenco dei file che hanno la
+stessa somma MD5 (i.e. l'output del comando md5sum).
+In output ogni riga deve mostrare un elenco di pathname realtivi a file che hanno la stessa somma MD5 (che quindi
+sono molto molto probabilmente uguali).'''
+
+
 from collections import defaultdict
 import sys
 import os
